d6401ab90389f19a303172c8c2297803456d7897
ifTNT <happyjohn369@gmail.com>
2018-05-15 23:39:54 +0800

    Change strategy

--- withgpu.js
+++ withgpu.js
@@ -1,18 +1,20 @@
 var Fractal = function(){
+    console.log("Strat " + window.performance.now());
     this.transforms = [
         [[-0.86,0.03,0],[-0.03,0.86,1.5],[0,0,1]], //T1
         [[0.2,-0.25,0],[0.25,0.26,0.45],[0,0,1]],  //T0
         [[-0.15,0.27,0],[0.25,0.26,0.45],[0,0,1]], //T3
         [[0,0,0],[0,0.17,0],[0,0,1]]               //T4
     ];
-    this.gpu = new GPU();
-    this.iterPoints = this.gpu.createKernel(function(p,t){
+    this.workLength = 9; // Actually length of work: 4^workLength
+    const gpu = new GPU();
+    this.iterPoints = gpu.createKernel(function(p,t,workLength){
         var T = 0;
-        if((this.thread.y/1024) <= 850){
+        if((this.thread.y/workLength) <= 0.83){
             T = 0;
-        }else if((this.thread.y/1024) <= 932){
+        }else if((this.thread.y/workLength) <= 0.91){
             T = 1;
-        }else if((this.thread.y/1024) <= 1014){
+        }else if((this.thread.y/workLength) <= 0.99){
             T = 2;
         }else{
             T = 3;
@@ -22,40 +24,43 @@ var Fractal = function(){
             sum += p[this.thread.y][i] * t[T][this.thread.x][i];
         }
         return sum;
-    }).setOutput([3,1024*1024]);
+    }).setOutput([3,Math.pow(4,this.workLength)]);
     this.genIV();
     console.log(this.IV);
 }
 Fractal.prototype.genIV = function(){
-    this.IV = [];
-    var tempIV = [];
-    var genTemp = function(_p,t){
-        if(t==5) return [_p];
+    this.IV = []; //Initial vector (A 3 by (4^workLength) matrix actually)
+    //var tempIV = [];
+    var genTemp = function(_p,t,workLength){
+        if(t==workLength) return [_p];
         var rtVal = [];
         var p;
         // T1
         p = [_p[0]*-0.86+_p[1]*0.03,  _p[0]*-0.03+_p[1]*0.86+1.5,  1];
-        rtVal = rtVal.concat(genTemp(p,t+1));
+        rtVal = rtVal.concat(genTemp(p,t+1,workLength));
         // T2
         p = [_p[0]*0.2+_p[1]*-0.25,  _p[0]*0.25+_p[1]*0.26+0.45,  1];
-        rtVal = rtVal.concat(genTemp(p,t+1));
+        rtVal = rtVal.concat(genTemp(p,t+1,workLength));
         //T3
         p = [_p[0]*-0.15+_p[1]*0.27,  _p[0]*0.25+_p[1]*0.26+0.25,  1];
-        rtVal = rtVal.concat(genTemp(p,t+1));
+        rtVal = rtVal.concat(genTemp(p,t+1,workLength));
         //T4
         p = [0,  _p[1]*0.17,  1];
-        rtVal = rtVal.concat(genTemp(p,t+1));
+        rtVal = rtVal.concat(genTemp(p,t+1,workLength));
         return rtVal;
     };
-    tempIV = genTemp([0,0,1],0);
-    for(var i=0; i<1024; i++){
-        for(var j=0; j<1024; j++){
+    this.IV = genTemp([0,0,1],0,this.workLength);
+    /*for(var i=0; i<100; i++){
+        for(var j=0; j<Math.pow(4,this.workLength); j++){
             this.IV.push(tempIV[j]);
         }
-    }
+    }*/
+    console.log("IV generated " + window.performance.now());
 }
 Fractal.prototype.genPoints = function(){
-    return this.iterPoints(this.IV, this.transforms);
+    var rtVal = this.iterPoints(this.IV, this.transforms, Math.pow(4,this.workLength));
+    console.log("Transformed " + window.performance.now());
+    return rtVal;
 }
 
 
@@ -86,7 +91,7 @@ function init() {
     }
     ctx.closePath();
     ctx.stroke();
-    console.log(points);
+    console.log(points.length);
     render(0);
 }
 
